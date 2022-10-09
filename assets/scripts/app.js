7a242f5134bf725313e6af923d72eee0b8b11828
solssak <gswoong1012@gmail.com>
2022-10-09 20:33:14 +0900

    Try-catch 맛보기

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,15 +11,29 @@ const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
 const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
 const LOG_EVENT_GAME_OVER = "GAME_OVER";
 
-const enteredValue = parseInt(
-  prompt("Maximum life for you and the monster.", "100")
-);
-
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+function getMaxLifeValues() {
+  const enteredValue = parseInt(
+    prompt("Maximum life for you and the monster.", "100")
+  );
+
+  const parsedValue = parseInt(enteredValue);
+  if (isNaN(parsedValue) || parsedValue <= 0) {
+    throw { message: "Invalid user input, not a number!" };
+  }
+  return parsedValue;
+}
+
+let chosenMaxLife;
+
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+  console.log(error);
   chosenMaxLife = 100;
+  alert("You entered something wrong, default value of 100 was used.");
+  // throw error;
 }
 
 let currentMonsterHealth = chosenMaxLife;
